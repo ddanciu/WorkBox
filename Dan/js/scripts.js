62723e47491d2cebbbd6e5082174c1b86847359f
Dan Danciu <dan@ddanciu.ro>
2011-05-30 00:11:27 +0300

    more changes

--- Dan/js/scripts.js
+++ Dan/js/scripts.js
@@ -1,5 +1,11 @@
 
 $(function() {
+	
+	
+	window.setTimeout(function() {
+        $(&#39;body&#39;).removeClass(&#39;loading&#39;);
+    }, 10);
+	
 	$(".post-body img").load(function() {
 		$(this).width( $(this).width() > 400 ? 400 : $(this).width());
 	});
